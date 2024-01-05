465fa8e7da78817dd29882d2707e7a0e1f9bd7f6
Omar Alaa <omaraka1999@gmail.com>
2024-01-05 19:57:36 +0200

    Update EEPROM.c

--- EEPROM.c
+++ EEPROM.c
@@ -17,7 +17,7 @@
 void EEPROM_write(uint16_t addr, uint8_t data){
 	
 	while(READBIT(EECR,EEWE) == 1);		//Making sure that the EEPROM is not in writing stage
-	EEAR = addr;							//Address and data are correctly set up
+	EEAR = addr;						//Address and data are correctly set up
 	EEDR = data;
 	SETBIT(EECR,EEMWE);					//EEPROM master write enable
 	SETBIT(EECR,EEWE);					//EEPROM write enable
@@ -26,13 +26,13 @@ void EEPROM_write(uint16_t addr, uint8_t data){
 /*****************************************************************************
 * Function Name: EEPROM_read
 * Purpose      : Enable reading from the EEPROM
-* Parameters   : uint16_t add
-* Return value : uint8_t
+* Parameters   : Address to read from (uint16_t)
+* Return value : Value that the address holds (uint8_t)
 *****************************************************************************/
 
 uint8_t EEPROM_read(uint16_t addr){
 	while(READBIT(EECR,EEWE) == 1);		//Making sure that the EEPROM is not in writing stage//
-	EEAR = addr;							//Address is correctly set up//
+	EEAR = addr;						//Address is correctly set up//
 	SETBIT(EECR,EERE);					//EERE = EEPROM Read Enable//
 	return EEDR;						//Return the data read in the address//
 }
